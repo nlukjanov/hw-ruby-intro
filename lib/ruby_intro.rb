4228b5a685ecff5a6fc4059500746f6e163c391e
Nikita Lukjanov <nlukjanov@gmail.com>
2018-02-22 15:17:17 +0000

    part 3 complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -25,11 +25,25 @@ def starts_with_consonant? s
 end
 
 def binary_multiple_of_4? s
-
+  if ( s =~ /^[0-1]+$/ && s.to_i(2) % 4 ==0 )
+    return true
+  else
+    false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize isbn, price
+    raise ArgumentError if (isbn.empty? || price <=0)
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    "$%0.2f" % @price
+  end
 end
